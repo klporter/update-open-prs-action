a36eb88cb08e0f15a6f354d4692c943bbc1cfd9d
Kevin Porter <kevin.porter@salesforce.com>
2020-06-15 11:32:11 -0700

    debugging

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ try {
     console.log(`Updating open PRs with new milestone: ${newMilestone.title}`)
 
     pull.getOpenPullRequests().then(openPRs => {
-        openPRs.forEach(openPR => {
-            issue.updateIssueWithMilestone(openPR.number, newMilestone.number)
-                .then(() => console.log(`Finished adding milestone (${newMilestone.title}) to PR: ${openPR.title}`))
-        })
+        console.log(JSON.stringify(openPRs));
+        // openPRs.forEach(openPR => {
+        //     issue.updateIssueWithMilestone(openPR.number, newMilestone.number)
+        //         .then(() => console.log(`Finished adding milestone (${newMilestone.title}) to PR: ${openPR.title}`))
+        // })
     });
 } catch (error) {
     core.setFailed(error.message);
